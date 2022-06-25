6ca457ccd7c03752d13968dea81391db97f57c89
valentinadinnadedeiokai <valentinaokai@gmail.com>
2022-06-25 14:00:38 -0700

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(array){
+    let game = (possibility) => {
+        return (possibility.result == 'W')
+        }
+       
+    let score = array.find(game);
+    if (score === undefined){
+        return undefined;
+    }else{
+        return score.year;
+    }
+   
+}
